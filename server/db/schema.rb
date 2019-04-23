f50a9df815d37aa6ef4d156e9722ba8a6461a9bf
Seth Paye <spaye@skookum.com>
2019-04-23 16:24:26 -0400

    test if deploys correctly with release part commented out

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_23_201022) do
+ActiveRecord::Schema.define(version: 2019_04_23_202317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 2019_04_23_201022) do
   end
 
   create_table "people", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
