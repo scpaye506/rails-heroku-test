8a0836091cbb35377275aa9f6528fee22a4e3142
Seth Paye <spaye@skookum.com>
2019-04-23 12:29:47 -0400

    make rails serve SPA work

--- server/app/controllers/todos_controller.rb
+++ server/app/controllers/todos_controller.rb
@@ -38,6 +38,12 @@ class TodosController < ApplicationController
     @todo.destroy
   end
 
+  def fallback_index_html
+    puts 'what now'
+    puts 'but why'
+    render :file => 'public/index.html'
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_todo
